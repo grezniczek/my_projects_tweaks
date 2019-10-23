9805e2e4fa92ac3a8d630a7ac487ef8d85e4df11
G. Rezniczek <g.rezniczek@gmail.com>
2019-10-23 17:04:32 +0200

    Remove column fix (had no business here)

--- js/replace-popup.js
+++ js/replace-popup.js
@@ -9,6 +9,4 @@ $(function() {
         img.removeClass('pnpimg')
         span.replaceWith('<span class="tooltip">' + span.html() + '<span class="tooltiptext">' + text + '</span></span>')
     })
-    var cols = $('div.hDiv div table tbody tr th').length
-    $('td.fldrrwparent').attr('colspan', cols);
 })
