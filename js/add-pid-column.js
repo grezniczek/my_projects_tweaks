cd512931f224f3b66518bfde627e6ddba17e5e10
G. Rezniczek <g.rezniczek@gmail.com>
2020-02-20 13:45:04 +0100

    Enhancements

--- js/add-pid-column.js
+++ js/add-pid-column.js
@@ -1,8 +1,7 @@
 // @ts-check
 if (typeof $ != 'undefined') $(function() {
     // Construct base link to project settings.
-    var version = $('#footer a[href="https://projectredcap.org"]').text().split(' ')[1]
-    var baselink = location.href.split('index.php?')[0] + 'redcap_v' + version + '/ControlCenter/edit_project.php?project='
+    var baselink = window['app_path_webroot'] + 'ControlCenter/edit_project.php?project='
     // Header.
     var THr = $('div.hDiv div table tbody tr th')[1]
     var THp = $('<th align="center"><div style="text-align:center;width:52px;">PID</div></th>')
@@ -10,10 +9,8 @@ if (typeof $ != 'undefined') $(function() {
     // Rows.
     $('[class^="pid-cntr-"]').each(function() {
         var pid = this.className.split('-').reverse()[0]
-        // @ts-ignore
-        var link = super_user ? ' href="' + baselink + pid + '"' : ''
-        // @ts-ignore
-        var tag = super_user ? 'a' : 'span'
+        var link = window['super_user'] ? ' href="' + baselink + pid + '"' : ''
+        var tag = window['super_user'] ? 'a' : 'span'
         var TDr = $(this).closest('td')
         var TDp = $('<td align="center"><div class="fc" style="width:52px; padding-left:10px;"><' + tag + link + '><span class="pid-cnt-h">' + pid + '</span>' + pid + '</' + tag +'></div></td>')
         $(TDp).attr('style', TDr.attr('style'))
