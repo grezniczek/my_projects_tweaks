108e9e9b078350b62ec04a8034e24c234e0b7e82
Günther Rezniczek <g.rezniczek@gmail.com>
2020-04-27 21:29:09 +0200

    Add comments

--- js/add-organize-filter.js
+++ js/add-organize-filter.js
@@ -6,20 +6,20 @@ if (typeof $ != 'undefined') $(function() {
     window.getProjectFolders = function() {
         orig()
         var $filter = $('[data-my-project-tweaks="organize-filter"]')
+        // Only initialize once.
         if (!$filter.length) {
             // Find the places.
             var $tr = $('#select_folders').parent().parent()
             var $tbody = $tr.parent()
+            // Remove checkbox (will re-add later).
             $('#hide_assigned').parent().remove()
+            // Assemble new elements.
             var $new_tr = $('<tr><td style="padding-top:7px;"><input id="hide_assigned" onclick="hideAssigned();" type="checkbox"><span style="font-size:11px;color:#000;font-weight:normal;">Hide projects already assigned</span></td><td></td></tr>')
             $tbody.append($new_tr)
             var $td = $('<td><input data-my-project-tweaks="organize-filter" type="text" class="x-form-text x-form-field" placeholder="Filter projects" style="margin-top:8px;max-width:200px;"> <button style="margin-top:6px;" class="btn btn-defaultrc btn-xs">&times;</button></td>')
             $tr.append($td)
             $filter = $td.find('input')
-            $td.find('button').on('click', function() {
-                $filter.val('')
-                $filter.trigger('change')
-            })
+            // The actual filter logic.
             function applyFilter() {
                 var filter = $filter.val().toString().toLowerCase()
                 $('#projects tr').each(function() {
@@ -31,9 +31,15 @@ if (typeof $ != 'undefined') $(function() {
                     }
                 })
                 if (filter != '') {
+                    // We don't want the "add all" when we filter. Add all is dangerous .. maybe add an option to suppress this?
                     $('#projects td.header').parent().hide()
                 }
             }
+            // Hook up events.
+            $td.find('button').on('click', function() {
+                $filter.val('')
+                $filter.trigger('change')
+            })
             $filter.on('keyup change', applyFilter)
             $('#projects').bind('DOMSubtreeModified', function(e) {
                 if (e.target.innerHTML.length > 0) {
